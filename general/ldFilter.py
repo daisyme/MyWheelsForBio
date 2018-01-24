0b0d47827f521ec5b08c039ce79399780bcd8c2e
Yuan Tao <ytao7@uci.edu>
2018-01-24 07:07:19 -0800

    small fix extra lines

--- general/ldFilter.py
+++ general/ldFilter.py
@@ -7,7 +7,7 @@ r1=int(sys.argv[2])
 r2=int(sys.argv[3])
 
 column=ld_file.readline()
-print >> filter_file, column
+print >> filter_file, column.strip()
 
 for rows in ld_file:
 	row=rows.split()
@@ -15,7 +15,7 @@ for rows in ld_file:
 	l2=int(row[2])
 	if l1 < r1 or l1 > r2 or l2 < r1 or l2 > r2:
 		continue
-	print >> filter_file, rows
+	print >> filter_file, rows.strip()
 
 ld_file.close()
 filter_file.close()
